2458c9ea321a0651a3cf4edc2344ffb95f8fa98f
Goodlookinguy <Goodlookinguy@nrgs.org>
2015-07-31 23:59:54 -0700

    - Added post format changer

---
+++ post-formater/post-format-changer.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Post Format Changer
+// @namespace    NRGsoft
+// @version      1.0
+// @description  Adds post format changer
+// @author       NRGLG
+// @run-at       document-end
+// @match        http://perfectworld.vanillaforums.com/*
+// @grant        none
+// ==/UserScript==
+
+(function()
+{
+    var formatDropdown = '<select id="Form_Format" name="Format">' +
+                            '<option value="BBCode">BBCode</option>' +
+                            '<option value="Markdown">Markdown</option>' +
+                            '<option value="Html">HTML</option>' +
+                            '<option value="Text">Text</option>' +
+                         '</select>';
+    
+    document.addEventListener("DOMNodeInserted", function(e)
+    {
+        var $target = $(e.target);
+        if ($target.hasClass('MessageForm'))
+            makePostFormatChanger(e.target);
+        
+    }, false);
+    
+    
+    var makePostFormatChanger = function(target)
+    {
+        var $target = $(target);
+        var $bodyboxWrap = $(($target.find('.bodybox-wrap'))[0]);
+        var $formFormat = $(($bodyboxWrap.find('#Form_Format'))[0]);
+        var $editorButtons = $(($bodyboxWrap.find('.editor'))[0]);
+        var formFormat = $formFormat.val();
+        $formFormat.remove();
+
+        var $formatDropdown = $(formatDropdown);
+        $editorButtons.prepend($formatDropdown);
+
+        $option = $(($formatDropdown.find('option[value="' + formFormat + '"]'))[0])
+        $option.attr('selected', 'selected');
+    };
+    
+    $('.FormWrapper').each(function(){makePostFormatChanger(this);});
+})();
